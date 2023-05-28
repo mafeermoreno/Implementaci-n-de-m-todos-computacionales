0d91a66da3da12a1037afe1ee353bcf2590c516f
mafeermoreno <a01708653@tec.mx>
2023-05-28 12:39:41 -0600

    Agregar status de output

--- AFD_A01708653_A01709413/RegularExpressionToAFD.cpp
+++ AFD_A01708653_A01709413/RegularExpressionToAFD.cpp
@@ -1,10 +1,3 @@
-// ==========================================================================
-// File: RegularExpressionToAFD.cpp
-// Author: María Fernanda Moreno Gómez A01708653
-//         Uri Jared Gopar Morales  A01709413
-// Description: Este archivo contiene el código para un AFD de una expresión regular
-//              To compile: g++ -std=c++17 RegularExpressionToAFD.cpp -o app   y después  ./app
-// ===========================================================================================
 #include <iostream>
 #include <fstream>
 #include <stack>
@@ -15,42 +8,34 @@
 
 using namespace std;
 
-// Definición de la estructura State que representa un estado del autómata
 struct State {
-    unordered_map<char, set<int>> transitions;  // Transiciones del estado, mapeo de caracteres a conjuntos de estados de destino
-    bool accepting;  // Indica si el estado es de aceptación
+    unordered_map<char, set<int>> transitions;
+    bool accepting;
 };
 
-// Definición de la clase Automata que representa el autómata finito determinista (AFD)
 class Automata {
 public:
-    vector<State> states;  // Vector de estados del AFD
+    vector<State> states;
 
-    // Constructor para un nuevo estado del AFD que representa un carácter específico
     Automata(char c, int) {
-        states.push_back(State{{}, false});  // Estado inicial
-        states.push_back(State{{}, true});   // Estado de aceptación
+        states.push_back(State{{}, false});
+        states.push_back(State{{}, true});
 
-        // Agregar transición desde el estado inicial al estado de aceptación usando el carácter especificado
         if (c != 'E') {
             states[0].transitions[c].insert(1);
         }
     }
 
-    // Constructor para un AFD con un número específico de estados
     Automata(size_t numStates) : states(numStates) {}
 
-    // Agregar una transición desde un estado dado hacia un estado de destino usando un carácter específico
     void addTransition(int from, int to, char c) {
         states[from].transitions[c].insert(to);
     }
 };
 
-// Función para concatenar dos autómatas
 Automata concatenateAutomata(const Automata& a, const Automata& b) {
     Automata result(size_t(a.states.size() + b.states.size()));
 
-    // Copiar las transiciones del primer autómata al autómata resultado
     for (int i = 0; i < a.states.size(); i++) {
         for (auto& [c, next] : a.states[i].transitions) {
             for (int to : next) {
@@ -59,7 +44,6 @@ Automata concatenateAutomata(const Automata& a, const Automata& b) {
         }
     }
 
-    // Copiar las transiciones del segundo autómata al autómata resultado, ajustando los índices de los estados
     for (int i = 0; i < b.states.size(); i++) {
         for (auto& [c, next] : b.states[i].transitions) {
             for (int to : next) {
@@ -68,19 +52,15 @@ Automata concatenateAutomata(const Automata& a, const Automata& b) {
         }
     }
 
-    // Actualizar el estado de aceptación del último estado del primer autómata
     result.states[a.states.size() - 1].accepting = false;
-    // Agregar una transición epsilon desde el último estado del primer autómata hacia el primer estado del segundo autómata
     result.addTransition(a.states.size() - 1, a.states.size(), 'E');
 
     return result;
 }
 
-// Función para realizar la unión de dos autómatas
 Automata unionAutomata(const Automata& a, const Automata& b) {
     Automata result(size_t(a.states.size() + b.states.size() + 2));
 
-    // Copiar las transiciones del primer autómata al autómata resultado, ajustando los índices de los estados
     for (int i = 0; i < a.states.size(); i++) {
         for (auto& [c, next] : a.states[i].transitions) {
             for (int to : next) {
@@ -89,7 +69,6 @@ Automata unionAutomata(const Automata& a, const Automata& b) {
         }
     }
 
-    // Copiar las transiciones del segundo autómata al autómata resultado, ajustando los índices de los estados
     for (int i = 0; i < b.states.size(); i++) {
         for (auto& [c, next] : b.states[i].transitions) {
             for (int to : next) {
@@ -98,11 +77,8 @@ Automata unionAutomata(const Automata& a, const Automata& b) {
         }
     }
 
-    // Agregar transiciones epsilon desde el estado inicial hacia los estados iniciales de los autómatas a y b
     result.addTransition(0, 1, 'E');
     result.addTransition(0, 1 + a.states.size(), 'E');
-
-    // Actualizar los estados de aceptación del autómata resultado
     result.states[a.states.size()].accepting = false;
     result.states[a.states.size() + b.states.size()].accepting = false;
     result.states[a.states.size() + b.states.size() + 1].accepting = true;
@@ -110,11 +86,9 @@ Automata unionAutomata(const Automata& a, const Automata& b) {
     return result;
 }
 
-// Función para realizar la operación de estrella (cerradura de Kleene) sobre un autómata
 Automata starAutomata(const Automata& a) {
     Automata result(size_t(a.states.size() + 2));
 
-    // Copiar las transiciones del autómata original al autómata resultado, ajustando los índices de los estados
     for (int i = 0; i < a.states.size(); i++) {
         for (auto& [c, next] : a.states[i].transitions) {
             for (int to : next) {
@@ -123,20 +97,16 @@ Automata starAutomata(const Automata& a) {
         }
     }
 
-    // Agregar transiciones epsilon desde el estado inicial hacia el primer estado y desde el último estado hacia el estado de aceptación
     result.addTransition(0, 1, 'E');
     result.addTransition(0, a.states.size() + 1, 'E');
     result.addTransition(a.states.size(), a.states.size() + 1, 'E');
     result.addTransition(a.states.size(), 1, 'E');
-
-    // Actualizar los estados de aceptación del autómata resultado
     result.states[a.states.size()].accepting = false;
     result.states[a.states.size() + 1].accepting = true;
 
     return result;
 }
 
-// Función para construir un autómata a partir de una expresión regular
 Automata constructAutomataFromRegex(const string& regex) {
     stack<Automata> automataStack;
 
@@ -173,29 +143,22 @@ int main() {
         return 1;   // Detener el sistema
     }
 
-    // Leer la expresión regular desde el archivo
     string regex;
     getline(inFile, regex);
 
-    // Leer el alfabeto desde el archivo
     string alphabet;
     getline(inFile, alphabet);
 
     inFile.close();
 
-    // Construye el autómata a partir de la expresión regular
     Automata a = constructAutomataFromRegex(regex);
 
-    // Inicializa la matriz de adyacencia con -1 en lugar de 0
     vector<vector<int>> adjacencyMatrix(a.states.size(), vector<int>(alphabet.length(), -1));
 
-    // Generar la matriz de adyacencia del AFD
     for (int i = 0; i < a.states.size(); i++) {
         for (auto& [c, next] : a.states[i].transitions) {
             for (int to : next) {
-                // Encuentra la posición del carácter en el alfabeto
                 int position = alphabet.find(c);
-                // Si el carácter está en el alfabeto, almacena el estado de destino en la matriz de adyacencia
                 if (position != string::npos) {
                     adjacencyMatrix[i][position] = to;
                 }
@@ -203,41 +166,31 @@ int main() {
         }
     }
 
-    // Abrir el archivo de salida
-    ofstream outFile("output.txt");
-
-    // Escribir la matriz de adyacencia del AFD en el archivo de salida
-    outFile << "Matriz de adyacencia del AFD:\n";
-    outFile << "Estado  ";
+    cout << "Matriz de adyacencia del AFD:\n";
+    cout << "Estado  ";
     for (char c : alphabet) {
-        outFile << c << "   ";
+        cout << c << "   ";
     }
-    outFile << "\n";
+    cout << "\n";
 
     for (int i = 0; i < adjacencyMatrix.size(); i++) {
-        outFile << i << "       ";
+        cout << i << "       ";
         for (int j = 0; j < adjacencyMatrix[i].size(); j++) {
-            // Imprimir -1 si no hay transición, de lo contrario imprimir el estado de destino
             if (adjacencyMatrix[i][j] == -1) {
-                outFile << "-1  ";
+                cout << "-1  ";
             } else {
-                outFile << adjacencyMatrix[i][j] << "   ";
+                cout << adjacencyMatrix[i][j] << "   ";
             }
         }
-        outFile << "\n";
+        cout << "\n";
     }
 
-    // Escribir los estados de aceptación del AFD en el archivo de salida
-    outFile << "Estados de aceptación del AFD:\n";
+    cout << "Estados de aceptación del AFD:\n";
     for (int i = 0; i < a.states.size(); i++) {
         if (a.states[i].accepting) {
-            outFile << i << "\n";
+            cout << i << "\n";
         }
     }
-
-    // Cerrar el archivo de salida
-    outFile.close();
 
     return 0;
 }
-
