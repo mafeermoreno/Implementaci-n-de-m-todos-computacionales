48e641dad69dbe97e05e53325318b266f234a619
mafeermoreno <a01708653@tec.mx>
2023-05-28 14:24:20 -0600

    Arreglar errores

--- AFD_A01708653_A01709413/RegularExpressionToAFD.cpp
+++ AFD_A01708653_A01709413/RegularExpressionToAFD.cpp
@@ -14,223 +14,198 @@
 #include <unordered_map>
 
 using namespace std;
-
-// Definición de la estructura State que representa un estado del autómata
 struct State {
-    unordered_map<char, set<int>> transitions;  // Transiciones del estado, mapeo de caracteres a conjuntos de estados de destino
+    unordered_map<char, char> transitions;  // Transiciones del estado
     bool accepting;  // Indica si el estado es de aceptación
 };
 
-// Definición de la clase Automata que representa el autómata finito determinista (AFD)
 class Automata {
 public:
     vector<State> states;  // Vector de estados del AFD
 
-    // Constructor para un nuevo estado del AFD que representa un carácter específico
-    Automata(char c, int) {
+    Automata(char c) {
         states.push_back(State{{}, false});  // Estado inicial
         states.push_back(State{{}, true});   // Estado de aceptación
 
-        // Agregar transición desde el estado inicial al estado de aceptación usando el carácter especificado
         if (c != 'E') {
-            states[0].transitions[c].insert(1);
+            states[0].transitions[c] = 'B';
         }
     }
 
-    // Constructor para un AFD con un número específico de estados
     Automata(size_t numStates) : states(numStates) {}
 
-    // Agregar una transición desde un estado dado hacia un estado de destino usando un carácter específico
-    void addTransition(int from, int to, char c) {
-        states[from].transitions[c].insert(to);
+    void addTransition(char from, char to, char c) {
+        states[from - 'A'].transitions[c] = to;
+    }
+
+    void printAutomata() {
+        for (size_t i = 0; i < states.size(); i++) {
+            cout << "State " << static_cast<char>(i + 'A') << (states[i].accepting ? " (Accepting)" : "") << ":\n";
+            for (auto& [c, to] : states[i].transitions) {
+                cout << "  --(" << c << ")--> " << to << '\n';
+            }
+        }
     }
 };
 
-// Función para concatenar dos autómatas
 Automata concatenateAutomata(const Automata& a, const Automata& b) {
-    Automata result(size_t(a.states.size() + b.states.size()));
+    Automata result(a.states.size() + b.states.size());
 
-    // Copiar las transiciones del primer autómata al autómata resultado
-    for (int i = 0; i < a.states.size(); i++) {
-        for (auto& [c, next] : a.states[i].transitions) {
-            for (int to : next) {
-                result.addTransition(i, to, c);
-            }
+    for (char i = 'A'; i < 'A' + a.states.size(); i++) {
+        for (auto& [c, to] : a.states[i - 'A'].transitions) {
+            result.addTransition(i, to, c);
         }
     }
 
-    // Copiar las transiciones del segundo autómata al autómata resultado, ajustando los índices de los estados
-    for (int i = 0; i < b.states.size(); i++) {
-        for (auto& [c, next] : b.states[i].transitions) {
-            for (int to : next) {
-                result.addTransition(i + a.states.size(), to + a.states.size(), c);
-            }
+    for (char i = 'A'; i < 'A' + b.states.size(); i++) {
+        for (auto& [c, to] : b.states[i - 'A'].transitions) {
+            result.addTransition(i + a.states.size(), to + a.states.size(), c);
         }
     }
 
-    // Actualizar el estado de aceptación del último estado del primer autómata
     result.states[a.states.size() - 1].accepting = false;
-    // Agregar una transición epsilon desde el último estado del primer autómata hacia el primer estado del segundo autómata
-    result.addTransition(a.states.size() - 1, a.states.size(), 'E');
+    result.addTransition(a.states.size() - 1 + 'A', a.states.size() + 'A', 'E');
 
     return result;
 }
 
-// Función para realizar la unión de dos autómatas
 Automata unionAutomata(const Automata& a, const Automata& b) {
-    Automata result(size_t(a.states.size() + b.states.size() + 2));
+    Automata result(a.states.size() + b.states.size() + 2);
 
-    // Copiar las transiciones del primer autómata al autómata resultado, ajustando los índices de los estados
-    for (int i = 0; i < a.states.size(); i++) {
-        for (auto& [c, next] : a.states[i].transitions) {
-            for (int to : next) {
-                result.addTransition(i + 1, to + 1, c);
-            }
+    for (char i = 'A'; i < 'A' + a.states.size(); i++) {
+        for (auto& [c, to] : a.states[i - 'A'].transitions) {
+            result.addTransition(i + 1, to + 1, c);
         }
     }
 
-    // Copiar las transiciones del segundo autómata al autómata resultado, ajustando los índices de los estados
-    for (int i = 0; i < b.states.size(); i++) {
-        for (auto& [c, next] : b.states[i].transitions) {
-            for (int to : next) {
-                result.addTransition(i + 1 + a.states.size(), to + 1 + a.states.size(), c);
-            }
+    for (char i = 'A'; i < 'A' + b.states.size(); i++) {
+        for (auto& [c, to] : b.states[i - 'A'].transitions) {
+            result.addTransition(i + 1 + a.states.size(), to + 1 + a.states.size(), c);
         }
     }
 
-    // Agregar transiciones epsilon desde el estado inicial hacia los estados iniciales de los autómatas a y b
-    result.addTransition(0, 1, 'E');
-    result.addTransition(0, 1 + a.states.size(), 'E');
+    result.addTransition('A', 'B', 'E');
+    result.addTransition('A', a.states.size() + 'B', 'E');
 
-    // Actualizar los estados de aceptación del autómata resultado
     result.states[a.states.size()].accepting = false;
     result.states[a.states.size() + b.states.size()].accepting = false;
-    result.states[a.states.size() + b.states.size() + 1].accepting = true;
+
+    result.addTransition(a.states.size() + 'A', result.states.size() - 1 + 'A', 'E');
+    result.addTransition(a.states.size() + b.states.size() + 'A', result.states.size() - 1 + 'A', 'E');
 
     return result;
 }
 
-// Función para realizar la operación de estrella (cerradura de Kleene) sobre un autómata
 Automata starAutomata(const Automata& a) {
-    Automata result(size_t(a.states.size() + 2));
+    Automata result(a.states.size() + 2);
 
-    // Copiar las transiciones del autómata original al autómata resultado, ajustando los índices de los estados
-    for (int i = 0; i < a.states.size(); i++) {
-        for (auto& [c, next] : a.states[i].transitions) {
-            for (int to : next) {
-                result.addTransition(i + 1, to + 1, c);
-            }
+    for (char i = 'A'; i < 'A' + a.states.size(); i++) {
+        for (auto& [c, to] : a.states[i - 'A'].transitions) {
+            result.addTransition(i + 1, to + 1, c);
         }
     }
 
-    // Agregar transiciones epsilon desde el estado inicial hacia el primer estado y desde el último estado hacia el estado de aceptación
-    result.addTransition(0, 1, 'E');
-    result.addTransition(0, a.states.size() + 1, 'E');
-    result.addTransition(a.states.size(), a.states.size() + 1, 'E');
-    result.addTransition(a.states.size(), 1, 'E');
+    result.addTransition('A', 'B', 'E');
+    result.addTransition('A', result.states.size() - 1 + 'A', 'E');
 
-    // Actualizar los estados de aceptación del autómata resultado
     result.states[a.states.size()].accepting = false;
-    result.states[a.states.size() + 1].accepting = true;
+
+    result.addTransition(a.states.size() + 'A', 'B', 'E');
+    result.addTransition(a.states.size() + 'A', result.states.size() - 1 + 'A', 'E');
 
     return result;
 }
 
-// Función para construir un autómata a partir de una expresión regular
-Automata constructAutomataFromRegex(const string& regex) {
-    stack<Automata> automataStack;
+string infixToPostfixRegex(string infixRegex) {
+    stack<char> operatorStack;
+    string postfixRegex;
 
-    for (char c : regex) {
-        if (c == '*') {
-            Automata a = automataStack.top();
-            automataStack.pop();
-            automataStack.push(starAutomata(a));
-        } else if (c == '+') {
-            Automata b = automataStack.top();
-            automataStack.pop();
-            Automata a = automataStack.top();
-            automataStack.pop();
-            automataStack.push(unionAutomata(a, b));
-        } else if (c == '.') {
-            Automata b = automataStack.top();
-            automataStack.pop();
-            Automata a = automataStack.top();
-            automataStack.pop();
-            automataStack.push(concatenateAutomata(a, b));
-        } else {
-            automataStack.push(Automata(c, 0));
+    unordered_map<char, int> precedence;
+    precedence['*'] = 3;
+    precedence['.'] = 2;
+    precedence['|'] = 1;
+    precedence['+'] = 1;
+
+    for (char c : infixRegex) {
+        switch (c) {
+            case '*':
+            case '.':
+            case '|':
+            case '+':
+                while (!operatorStack.empty() && precedence[operatorStack.top()] >= precedence[c]) {
+                    postfixRegex += operatorStack.top();
+                    operatorStack.pop();
+                }
+                operatorStack.push(c);
+                break;
+            case '(':
+                operatorStack.push(c);
+                break;
+            case ')':
+                while (operatorStack.top() != '(') {
+                    postfixRegex += operatorStack.top();
+                    operatorStack.pop();
+                }
+                operatorStack.pop();  // Pop '('
+                break;
+            default:  // Operand
+                postfixRegex += c;
+                break;
         }
     }
 
-    return automataStack.top();
+    while (!operatorStack.empty()) {
+        postfixRegex += operatorStack.top();
+        operatorStack.pop();
+    }
+
+    return postfixRegex;
+}
+
+Automata constructAutomataFromRegex(string postfixRegex) {
+    stack<Automata> s;
+
+    for (char c : postfixRegex) {
+        switch (c) {
+            case '*':
+                s.push(starAutomata(s.top()));
+                s.pop();
+                break;
+            case '|':
+            case '+':
+                {
+                Automata b = s.top(); s.pop();
+                Automata a = s.top(); s.pop();
+                s.push(unionAutomata(a, b));
+                }
+                break;
+            case '.':
+                {
+                Automata b = s.top(); s.pop();
+                Automata a = s.top(); s.pop();
+                s.push(concatenateAutomata(a, b));
+                }
+                break;
+            default:
+                s.push(Automata(c));
+                break;
+        }
+    }
+
+    return s.top();
 }
 
 int main() {
-    ifstream inFile("input.txt");
+    ifstream file("input.txt");
 
-    if (!inFile) {
-        cerr << "No se pudo abrir input.txt";
-        return 1;   // Detener el sistema
-    }
+    string infixRegex, alphabet;
+    file >> infixRegex >> alphabet;
 
-    // Leer la expresión regular desde el archivo
-    string regex;
-    getline(inFile, regex);
+    string postfixRegex = infixToPostfixRegex(infixRegex);
 
-    // Leer el alfabeto desde el archivo
-    string alphabet;
-    getline(inFile, alphabet);
+    Automata a = constructAutomataFromRegex(postfixRegex);
 
-    inFile.close();
-
-    // Construye el autómata a partir de la expresión regular
-    Automata a = constructAutomataFromRegex(regex);
-
-    // Inicializa la matriz de adyacencia con -1 en lugar de 0
-    vector<vector<int>> adjacencyMatrix(a.states.size(), vector<int>(alphabet.length(), -1));
-
-    // Generar la matriz de adyacencia del AFD
-    for (int i = 0; i < a.states.size(); i++) {
-        for (auto& [c, next] : a.states[i].transitions) {
-            for (int to : next) {
-                // Encuentra la posición del carácter en el alfabeto
-                int position = alphabet.find(c);
-                // Si el carácter está en el alfabeto, almacena el estado de destino en la matriz de adyacencia
-                if (position != string::npos) {
-                    adjacencyMatrix[i][position] = to;
-                }
-            }
-        }
-    }
-
-    // Escribir la matriz de adyacencia del AFD en la salida estándar
-    cout << "Matriz de adyacencia del AFD:\n";
-    cout << "Estado  ";
-    for (char c : alphabet) {
-        cout << c << "   ";
-    }
-    cout << "\n";
-
-    for (int i = 0; i < adjacencyMatrix.size(); i++) {
-        cout << i << "       ";
-        for (int j = 0; j < adjacencyMatrix[i].size(); j++) {
-            // Imprimir -1 si no hay transición, de lo contrario imprimir el estado de destino
-            if (adjacencyMatrix[i][j] == -1) {
-                cout << "-1  ";
-            } else {
-                cout << adjacencyMatrix[i][j] << "   ";
-            }
-        }
-        cout << "\n";
-    }
-
-    // Escribir los estados de aceptación del AFD en la salida estándar
-    cout << "Estados de aceptación del AFD:\n";
-    for (int i = 0; i < a.states.size(); i++) {
-        if (a.states[i].accepting) {
-            cout << i << "\n";
-        }
-    }
+    a.printAutomata();
 
     return 0;
 }
